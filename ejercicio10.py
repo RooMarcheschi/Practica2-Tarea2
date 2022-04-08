5383ea349a473fa7bd2607731f65781e5aeebf91
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2022-04-08 20:56:13 -0300

    Agregue ejercicio10.py.
    Agregue ejercicio10.py completo y funcionando!

---
+++ ejercicio10.py
@@ -0,0 +1,23 @@
+with open('nombres_1.txt', 'r') as name:
+    names = name.read().split(',')
+with open('eval1.txt', 'r') as grade1:
+    grades1 = grade1.read().split(',')
+with open('eval2.txt', 'r') as grade2:
+    grades2 = grade2.read().split(',')
+
+dict = {}
+    #print(len(names), len(grades1), len(grades2)) --> 47
+avarage = 0
+for student in range(len(names)):
+    dict[str(names[student])] = int(grades1[student]) + int(grades2[student])
+    avarage = avarage + int(grades1[student]) + int(grades2[student])
+
+#for student in dict:
+#    print(student, dict[student])
+avarage = avarage//len(names)
+print("The avarage grade is ", avarage)
+print("And the students with grades under the avarage are: ")
+for student in dict:
+    if dict[student] < avarage:
+        print(student, dict[student])
+
