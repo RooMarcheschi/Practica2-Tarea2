729a60cb09582837c33bb4029a1f7884bcc9bb78
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2022-04-12 00:36:54 -0300

    Modifique ejercicios 10 y 11

--- ejercicio10.py
+++ ejercicio10.py
@@ -5,19 +5,24 @@ with open('eval1.txt', 'r') as grade1:
 with open('eval2.txt', 'r') as grade2:
     grades2 = grade2.read().split(',')
 
-dict = {}
+#dict = {}
     #print(len(names), len(grades1), len(grades2)) --> 47
+
+names_grades = []
 avarage = 0
-x = len(names)
-for student in range(x):
-    dict[str(names[student])] = int(grades1[student]) + int(grades2[student])
+for student in range(len(names)):
+    names_grades.append([str(names[student]), int(grades1[student]) + int(grades2[student])])
+    #dict[str(names[student])] = int(grades1[student]) + int(grades2[student])
     avarage = avarage + int(grades1[student]) + int(grades2[student])
 
 #for student in dict:
 #    print(student, dict[student])
 avarage = avarage//len(names)
+print(len(names), len(names_grades))
 print("The avarage grade is ", avarage)
 print("And the students with grades under the avarage are: ")
-for student in dict:
-    if dict[student] < avarage:
-        print(student, dict[student])
+for student in names_grades:
+    if student[1] < avarage:
+        print(student[0], student[1])
+    #if names_grades[student][1] < avarage:
+    #    print(student, dict[student])
--- ejercicio11.py
+++ ejercicio11.py
@@ -2,11 +2,12 @@ with open('nombres1.txt', 'r') as name1:
     names1 = name1.read().split(',')
 with open('nombres2.txt', 'r') as name2:
     names2 = name2.read().split(',')
-#names1 = set(names1)
-#names2 = set(names2)
-#merge = names1.intersection(names2)
-merge = [name for name in names1 if name in names2]
-print("Nombres que aparecen en los archivos bombres1 y nombres2: ")
+
+namess1 = [i.strip() for i in names1]
+namess2 = [i.strip() for i in names2]
+
+merge = [name for name in namess1 if name in namess2]
+print("Nombres que aparecen en los archivos nombres1 y nombres2: ")
 for name in merge:
     print(name)
 
@@ -21,33 +22,8 @@ totals = []
 for i in range(len(grades1)):
     totals.append(int(grades1[i])+int(grades2[i]))
 
-#zipped = zip(names1, grades1, grades2, totals)
-#for student in zipped:
-#    print(student)
-
-#for i, (Nombres, Eval1, Eval2, Total) in enumerate(zip(names1, grades1, grades2, totals)):
-    #print(i, Nombres, Eval1, Eval2, Total)
-    #print(f"{int(i)}     {str(Nombres)}   {int(Eval1)}  {int(Eval2)}  {Total}")
-
-
-
-
 print("Impresion de notas de los alumnos en nombres1: ")
-list = []
-for student in range(len(names1)):
-    #list.append([str(names1[student]),int(grades1[student]),int(grades2[student]),int(grades1[student])+int(grades2[student])])
-    list.append(f"{names1[student]}   {int(grades1[student])}   {int(grades2[student])}   {int(grades1[student])+int(grades2[student])}")
-
-for elem in list:
-    print(elem)
-
-
-"""
-#print("      "+"Nombre  "+"    "+"Eval1   "+"Eval2   "+"Total   ")
-#for i in range(len(names1)):
-#    print(f"{i}     {list[i][0]}    {list[i][1]}     {list[i][2]}     {list[i][3]}")
-
-print ("{:<8} {:<15} {:<10} {:<10}".format('Pos','Nombre','Eval1','Eval2','Total'))
-for l in list:
-    Nombre, Eval1, Eval2, Total = l
-    print ("{:<4} {:<15} {:<10} {:<10} {:<10}".format(l, Nombre, Eval1, Eval2, Total))"""
+print(f"{'ID':<5}{'Nombre':<20}{'Eval1':<8}{'Eval2':<8}{'Total':<8}")
+print('-------------------------------------------------------')
+for i in range(len(namess1)):
+    print(f"{i:<5}{namess1[i]:<20}{int(grades1[i]):<8}{int(grades2[i]):<8}{int(totals[i]):<8}")
