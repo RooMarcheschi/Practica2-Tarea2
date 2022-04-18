c7390e1b40ebb0e8d283dc9c3d3a1547d2aa3182
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2022-04-18 20:40:54 -0300

    modificacion en ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -18,7 +18,7 @@ for student in range(len(names)):
 #for student in dict:
 #    print(student, dict[student])
 avarage = avarage//len(names)
-print(len(names), len(names_grades))
+#print(len(names), len(names_grades))
 print("The avarage grade is ", avarage)
 print("And the students with grades under the avarage are: ")
 for student in names_grades:
